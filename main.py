6e49351a77fbd6720a4843e549c5526b07eec03d
fscovino <franciscoscovino@gmail.com>
2024-12-10 21:35:57 -0500

    Adds os functionality

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import os
 
 df = pd.read_excel('./data/Superstore.xlsx')
 df_final = df.iloc[:10, 0:4]
 
-df_final.to_csv('./data/Superstore.csv', index=False)
+path = os.path.dirname(os.path.realpath(__file__))
+df_final.to_csv(f'{path}/data/Superstore.csv', index=False)
+
+print(f'Exporting to {path}/data/Superstore.csv')
 print(df_final)
 print('bye')
